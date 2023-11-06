70c8a027516d009c791199d7357cba9c6b8bebe8
nilima7864 <nilimaghawate@gmail.com>
2023-11-06 19:23:07 +0530

    user changed

--- Menta-MobAutomation-Project/src/test/java/TestNGRunner.java
+++ Menta-MobAutomation-Project/src/test/java/TestNGRunner.java
@@ -11,7 +11,7 @@ public class TestNGRunner {
 			glue = { "com.cucumber.stepdefinition" },
 			strict = false,
 			dryRun = false,
-				tags = {"@IM_Sanity"},
+				tags = {"@IM_WRU_Screen"},
 				     //@IM_AWS , @GL_AWS , @L_AWS
 					//AndroidMenta_Sanity
 				   // @GL_Sanity , @L_Sanity , @IM_Sanity 
